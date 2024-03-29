8f2386da9ed269b928c469d9da6bcea1b85a7fce
Monarch Wadia <monarchwadia@gmail.com>
2024-03-28 23:45:30 -0400

    LLM Function prototype

--- src/llm-functions/index.ts
+++ src/llm-functions/index.ts
@@ -1,3 +1,31 @@
+import { OpenAI } from "openai";
+const { VITE_OPENAI_CREDS } = import.meta.env;
+
+const fetchOpenAICompletion = async (prompt: string) => {
+  const o = new OpenAI({
+    apiKey: VITE_OPENAI_CREDS,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const response = await o.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+
+  const answer = response.choices[0].message.content;
+
+  return answer;
+};
+
 export const quickPredict = async (text: string) => {
-  return text;
+  return fetchOpenAICompletion(text);
 };
