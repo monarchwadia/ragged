1726122e8ff9e04df46993116c20bd92800f0095
Monarch Wadia <monarchwadia@gmail.com>
2024-03-29 21:11:49 -0400

    Updated example

--- examples/vite-browser/src/pages/root.tsx
+++ examples/vite-browser/src/pages/root.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { Ragged } from "ragged/unminified";
+import { Ragged } from "ragged";
+
+// can also import unminified
+// import { Ragged } from "ragged/unminified";
+
 const { VITE_OPENAI_CREDS } = import.meta.env;
 
 const l = new Ragged({
@@ -12,17 +16,49 @@ const l = new Ragged({
 function App() {
   const [prediction, setPrediction] = useState("");
 
-  const doPrediction = async () => {
+  const doPredictionPromiseBased = async () => {
     const p = await l.qPredict("What is toronto?");
     setPrediction(p);
+  };
+
+  const doPredictionEventDriven = async () => {
+    const p = l.predict("What is toronto?");
+    p.subscribe((e) => {
+      // the "started" event is emitted when the prediction starts
+      if (e.type === "started") {
+        // no-op
+      }
+
+      // WIP
+      // doesn't get emitted yet, but will in the future
+      // the "collected" event is emitted with the partially complete prediction as it streams down
+      if (e.type === "collected") {
+        setPrediction(e.payload);
+        // Toronto
+        // Toronto is
+        // Toronto is a
+        // Toronto is a city
+        // Toronto is a city in
+        // etc
+      }
+
+      // the "completed" event is emitted with the fully complete prediction
+      if (e.type === "finished") {
+        setPrediction(e.payload);
+        // Toronto is a city in Canada.
+      }
+    });
   };
 
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>{prediction}</div>
-      <div className="btn btn-success" onClick={doPrediction}>
-        Predict
+      <div className="btn btn-success" onClick={doPredictionEventDriven}>
+        Predict Streaming
+      </div>
+      <div className="btn btn-success" onClick={doPredictionPromiseBased}>
+        Predict Async
       </div>
     </div>
   );
