f25d0fed519261352a278f2ff7de840c56a874d0
Monarch Wadia <monarchwadia@gmail.com>
2024-04-01 17:42:41 -0400

    Now throwing tool use prediction events

--- ragged/src/predict.ts
+++ ragged/src/predict.ts
@@ -1,17 +1,34 @@
 import { Subject } from "rxjs";
 import type { OpenAI } from "openai";
 import { ChatCompletionDetector } from "../detector/ChatCompletionDetector";
+import { ToolUseCompletionDetector } from "../detector/ToolUseCompletionDetector";
 
 type LlmStreamEvent =
   | { type: "started"; index: number }
   | { type: "chunk"; index: number; payload: string }
   | { type: "collected"; index: number; payload: string }
-  | { type: "finished"; index: number; payload: string };
+  | { type: "finished"; index: number; payload: string }
+  | {
+      type: "tool_use_start";
+      index: { toolCallIndex: number; choiceIndex: number };
+      payload: {
+        name: string;
+      };
+    }
+  | {
+      type: "tool_use_finish";
+      index: { toolCallIndex: number; choiceIndex: number };
+      payload: {
+        name: string;
+        arguments: unknown;
+      };
+    };
 
 export const predictStream = (o: OpenAI, prompt: string) => {
   const operationEvents = new Subject<LlmStreamEvent>();
 
   const chatCompletionDetector = new ChatCompletionDetector();
+
   chatCompletionDetector.listen((evt) => {
     const { type, index } = evt;
 
@@ -36,6 +53,38 @@ export const predictStream = (o: OpenAI, prompt: string) => {
       case "CHAT_COMPLETION_FINISH":
         operationEvents.next({ type: "finished", index, payload: evt.content });
         operationEvents.complete();
+        break;
+    }
+  });
+
+  const toolUseCompletionDetector = new ToolUseCompletionDetector();
+
+  toolUseCompletionDetector.listen((evt) => {
+    switch (evt.type) {
+      case "TOOL_USE_START":
+        operationEvents.next({
+          type: "tool_use_start",
+          index: {
+            choiceIndex: evt.choiceIndex,
+            toolCallIndex: evt.toolCallIndex,
+          },
+          payload: {
+            name: evt.toolCall.name,
+          },
+        });
+        break;
+      case "TOOL_USE_FINISH":
+        operationEvents.next({
+          type: "tool_use_finish",
+          index: {
+            choiceIndex: evt.choiceIndex,
+            toolCallIndex: evt.toolCallIndex,
+          },
+          payload: {
+            name: evt.toolCall.name,
+            arguments: evt.toolCall.arguments,
+          },
+        });
         break;
     }
   });
