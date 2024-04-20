668c6c94cab3e8b0bc81b197d815931473d16ca7
Monarch Wadia <monarchwadia@gmail.com>
2024-04-19 22:43:02 -0400

    Now have basic spaceship

--- examples/2024-04-19-home-automation-ui/src/routes/+page.svelte
+++ examples/2024-04-19-home-automation-ui/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	type SpaceshipSection = {
+		id: number;
+		name: string;
+	};
+	const rooms: SpaceshipSection[] = [
+		{ id: 1, name: 'Command Bridge' },
+		{ id: 2, name: 'Engineering Bay' },
+		{ id: 3, name: 'Crew Quarters' }
+	];
+</script>
+
+<div class="flex flex-col w-fit gap-4 p-8">
+	<h1 class="text-3xl font-bold">Spaceship</h1>
+	{#each rooms as room}
+		<div class="card bg-base-200 card-compact card-bordered shadow-lg">
+			<div class="card-body">
+				<h5 class="card-title">{room.name}</h5>
+			</div>
+		</div>
+	{/each}
+</div>
