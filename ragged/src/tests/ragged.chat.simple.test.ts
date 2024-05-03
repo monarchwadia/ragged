986bd1cc2ddb5c3aa2e1950c8f0427849ad7f940
Monarch Wadia <monarchwadia@gmail.com>
2024-05-03 09:24:04 -0400

    Testing chat string

--- ragged/src/tests/ragged.chat.simple.test.ts
+++ ragged/src/tests/ragged.chat.simple.test.ts
@@ -1,12 +1,14 @@
 import { Ragged } from "../Ragged";
-import { TestDriver, helloWorldDriver } from "./TestDriver";
+import { helloWorldDriver } from "./TestDriver";
 
 describe("ragged.chat.simple", () => {
-  it("can chat with a string", async () => {
-    const r = new Ragged(helloWorldDriver());
+    it("can chat with a string", async () => {
+        let driver: any = helloWorldDriver();
+        jest.spyOn(driver, "chatStream");
+        const r = new Ragged(driver);
 
-    const history = await r.chat("").asHistory();
-    expect(history).toMatchInlineSnapshot(`
+        const history = await r.chat("dummy input string").asHistory();
+        expect(history).toMatchInlineSnapshot(`
       [
         {
           "data": {
@@ -17,5 +19,6 @@ describe("ragged.chat.simple", () => {
         },
       ]
     `);
-  });
+        expect(driver.chatStream).toHaveBeenCalledWith([{ "data": { "text": "dummy input string" }, "role": "human", "type": "history.text" }], undefined);
+    });
 });
