09b8a7fbe5f3c2b43a1b1e9074f0e3a50378355d
Monarch Wadia <monarchwadia@gmail.com>
2024-05-03 09:58:21 -0400

    cleaned up tests a little

---
+++ ragged/src/tests/ragged.chat.asHistory.ts
@@ -0,0 +1,34 @@
+import { Ragged } from "../Ragged";
+import { helloWorldDriver } from "./TestDriver";
+
+describe("ragged.chat.asHistory", () => {
+  it("returns with a correct response", async () => {
+    const driver = helloWorldDriver();
+
+    const r = new Ragged(driver);
+    const history = await r.chat("dummy input string").asHistory();
+
+    expect(history).toMatchInlineSnapshot(`
+        [
+          {
+            "data": {
+              "text": "Hello, world!",
+            },
+            "role": "ai",
+            "type": "history.text",
+          },
+        ]
+      `);
+  });
+
+  it("translates the string into a history object, which is then passed into the driver.", async () => {
+    let driver: any = helloWorldDriver();
+    jest.spyOn(driver, "chatStream");
+
+    const r = new Ragged(driver);
+    await r.chat("dummy input string").asHistory();
+
+    expect(driver.chatStream).toHaveBeenCalledWith([{ "data": { "text": "dummy input string" }, "role": "human", "type": "history.text" }], undefined);
+  });
+});
+
