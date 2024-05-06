ac119b70348c7c9db4ec9db003a8b3f1fb658966
Monarch Wadia <monarchwadia@gmail.com>
2024-05-05 23:33:40 -0400

    Deleted vite-browser example

--- examples/vite-browser/src/routes.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./pages/root.tsx";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
