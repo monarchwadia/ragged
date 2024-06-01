96f9d5646b01ae5df39e1906fdb813a4cbb0042e
Monarch Wadia <monarchwadia@gmail.com>
2024-06-01 19:17:01 -0400

    Added pollyjs

--- ragged/src/driver/OpenAi/OpenAiChatDriver.test.ts
+++ ragged/src/driver/OpenAi/OpenAiChatDriver.test.ts
@@ -1,32 +1,50 @@
+import { Polly } from '@pollyjs/core';
+import FetchAdapter from '@pollyjs/adapter-fetch';
+import FSPersister from "@pollyjs/persister-fs";
+
+/*
+  Register the adapters and persisters we want to use. This way all future
+  polly instances can access them by name.
+*/
+Polly.register(FetchAdapter);
+Polly.register(FSPersister);
+
 import { DriverApiClient } from "../DriverApiClient";
 import { OpenAiChatDriver } from "./OpenAiChatDriver";
-import nock from 'nock';
 
 describe("OpenAiChatDriver", () => {
     beforeEach(() => {
-        nock('https://api.openai.com')
-            .post('/v1/chat/completions')
-            .reply(200, {
-                "choices": [
-                    {
-                        "finish_reason": "stop",
-                        "index": 0,
-                        "logprobs": null,
-                        "text": "I am a chatbot."
-                    }
-                ]
-            });
-    })
+    });
+
+    afterEach(() => {
+
+    });
+
     it("should perform a chat completion", async () => {
         // Arrange
         const config = {
-            apiKey: process.env.OPENAI_API_KEY
+            // apiKey: process.env.OPENAI_API_KEY
         }
 
         const driverApiClient = new DriverApiClient();
         const driver = new OpenAiChatDriver(driverApiClient, config);
 
         // Act
+        const polly = new Polly('OpenAiChatDriver > should perform a chat completion', {
+            adapters: ['fetch'],
+            persister: 'fs',
+            matchRequestsBy: {
+                headers: false,
+                body: false,
+                order: false,
+            }
+        });
+        polly.server.any().on('beforeResponse', (req) => {
+            // mask auth header
+            if (req.headers['authorization']) {
+                req.headers['authorization'] = 'Bearer <redacted>';
+            }
+        });
         const result = await driver.chatCompletion({
             model: "gpt-3.5-turbo",
             messages: [
@@ -34,6 +52,8 @@ describe("OpenAiChatDriver", () => {
                 { role: "user", content: "What are you?" }
             ]
         });
+
+        await polly.stop();
 
         console.log(JSON.stringify(result));
     })
