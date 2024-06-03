8741b313be1ad55d9718624669baf43830cc92d7
Monarch Wadia <monarchwadia@gmail.com>
2024-06-03 02:08:48 -0400

    Chat with recording done

--- ragged/src/chat/Chat.test.ts
+++ ragged/src/chat/Chat.test.ts
@@ -5,30 +5,125 @@ import { DeepMockProxy, mockDeep } from "jest-mock-extended";
 
 describe("Chat", () => {
     let adapter: DeepMockProxy<BaseChatAdapter>;
-    let chat: Chat;
+    let c: Chat;
 
     beforeEach(() => {
         adapter = mockDeep<BaseChatAdapter>();
-        chat = new Chat(adapter);
+        c = new Chat(adapter);
     });
 
-    it("should be able to chat", async () => {
-        const history: Message[] = [
-            {
-                type: "bot",
-                text: "This is a test response from the adapter"
-            }
-        ];
+    describe("Default behaviour", () => {
+        it("Calls the adapter with the correct request", async () => {
+            adapter.chat.mockResolvedValue({ history: [] });
 
-        adapter.chat.mockResolvedValue({ history });
+            await c.chat(`This is a test message to the adapter`);
 
-        const response = await chat.chat([
-            {
-                type: "user",
-                text: "This is a test message to the adapter"
-            }
-        ]);
+            expect(adapter.chat).toHaveBeenCalledWith({
+                history: [
+                    {
+                        type: "user",
+                        text: "This is a test message to the adapter"
+                    }
+                ]
+            });
+        });
 
-        expect(response).toBe(history);
+        it("returns just the responses", async () => {
+            const expectedOutput: Message[] = [
+                {
+                    type: "bot",
+                    text: "This is a test response from the adapter"
+                }
+            ];
+
+            adapter.chat.mockResolvedValue({ history: expectedOutput });
+
+            const history = await c.chat(`This is a test message to the adapter`, [{
+                type: "system",
+                text: "This is a system message"
+            }]);
+
+            expect(history).toMatchObject([
+                ...expectedOutput
+            ] as Message[]);
+        });
+
+        it("can chat even without a history of messages", async () => {
+            const expectedOutput: Message[] = [
+                {
+                    type: "bot",
+                    text: "This is a test response from the adapter"
+                }
+            ];
+
+            adapter.chat.mockResolvedValue({ history: expectedOutput });
+
+            const history = await c.chat(`This is a test message to the adapter`);
+
+            expect(history).toMatchObject([
+                ...expectedOutput
+            ] as Message[]);
+        });
     });
+
+    describe("with recording", () => {
+        it("should return the history of messages", async () => {
+            c.record(true);
+
+            // adapter should return just the LLM response
+            adapter.chat.mockResolvedValue({
+                history: [
+                    {
+                        type: "bot",
+                        text: "This is a test response from the adapter"
+                    }
+                ]
+            });
+
+            const messages = await c.chat(`This is a test message to the adapter`);
+
+            expect(messages).toMatchObject([
+                {
+                    type: "user",
+                    text: `This is a test message to the adapter`
+                },
+                {
+                    type: "bot",
+                    text: "This is a test response from the adapter"
+                }
+            ] as Message[]);
+        });
+
+        it("should ignore the history passed in", async () => {
+            c.record(true);
+
+            // adapter should return just the LLM response
+            adapter.chat.mockResolvedValue({
+                history: [
+                    {
+                        type: "bot",
+                        text: "This is a test response from the adapter"
+                    }
+                ]
+            });
+
+            const messages = await c.chat(`This is a test message to the adapter`, [
+                {
+                    type: "user",
+                    text: "This message will be totally ignored"
+                }
+            ]);
+
+            expect(messages).toMatchObject([
+                {
+                    type: "user",
+                    text: `This is a test message to the adapter`
+                },
+                {
+                    type: "bot",
+                    text: "This is a test response from the adapter"
+                }
+            ] as Message[]);
+        });
+    })
 })
--- ragged/src/chat/index.ts
+++ ragged/src/chat/index.ts
@@ -4,19 +4,74 @@ import { provideOpenAiChatAdapter } from "./provider/openai";
 import { OpenAiChatDriverConfig } from "./provider/openai/driver";
 
 export class Chat {
+    private _history: Message[] = [];
+
+    private _isRecording: boolean = false;
     constructor(private adapter: BaseChatAdapter) { }
 
-    async chat(history: Message[]): Promise<Message[]> {
-        const request: ChatRequest = {
-            history
+    async chat(userMessage: string, history: Message[] = []): Promise<Message[]> {
+
+        // prepare the request
+
+        let requestHistory = [];
+        if (this._isRecording) {
+            // if recording, we ignore the history passed in and use the internal history
+            requestHistory = Chat.cloneMessages(this._history);
+        } else {
+            requestHistory = Chat.cloneMessages(history);
         }
 
+        requestHistory.push({
+            type: "user",
+            text: userMessage
+        });
+
+        // execute the request
+
+        const request: ChatRequest = {
+            history: requestHistory
+        }
+
+        // handle response
+
         const response = await this.adapter.chat(request);
-        return response.history;
+
+        let responseHistory: Message[] = [];
+        if (this._isRecording) {
+            responseHistory = request.history;
+        }
+
+        responseHistory.push(...response.history);
+
+        return Chat.cloneMessages(responseHistory);
+    }
+
+    public record(record: boolean) {
+        this._isRecording = record;
+    }
+
+    get isRecording() {
+        return this._isRecording;
+    }
+
+    set history(history: Message[]) {
+        this._history = Chat.cloneMessages(history);
+    }
+
+    get history() {
+        return Chat.cloneMessages(this._history)
     }
 
     static with(provider: "openai", config: Partial<OpenAiChatDriverConfig> = {}) {
         const adapter = provideOpenAiChatAdapter({ config })
         return new Chat(adapter);
+    }
+
+    private static cloneMessages(messages: Message[]): Message[] {
+        return messages.map(Chat.cloneMessage);
+    }
+
+    private static cloneMessage(message: Message): Message {
+        return { ...message };
     }
 }
