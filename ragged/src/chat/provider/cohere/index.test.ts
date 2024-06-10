8dba709b5f30cf5d10459e69e799914583d8e8be
Monarch Wadia <monarchwadia@gmail.com>
2024-06-09 20:30:54 -0400

    Created first cohere chat test

--- ragged/src/chat/provider/cohere/index.test.ts
+++ ragged/src/chat/provider/cohere/index.test.ts
@@ -1,4 +1,6 @@
 import { provideCohereChatAdapter } from ".";
+import { startPollyRecording } from "../../../../test/startPollyRecording";
+import { ChatRequest } from "../index.types";
 import { CohereChatAdapter } from "./CohereChatAdapter";
 
 describe("cohereChatAdapterProvider", () => {
@@ -7,13 +9,27 @@ describe("cohereChatAdapterProvider", () => {
         expect(adapter).toBeInstanceOf(CohereChatAdapter);
     });
 
-    // it("successfully performs a request", () => {
-    //     const adapter = provideCohereChatAdapter();
-    //     const request = {
-    //         message: "Hello, world!"
-    //     };
-    //     adapter.chat(request).then((response) => {
-    //         expect(response).toBeDefined();
-    //     });
-    // })
+    it("successfully performs a request", async () => {
+
+        const adapter = provideCohereChatAdapter();
+
+        const request: ChatRequest = {
+            history: [
+                {
+                    type: "user",
+                    text: "Hello, how are you?"
+                }
+            ]
+        }
+
+        const polly = startPollyRecording("cohereChatAdapterProvider > successfully performs a request");
+        const response = await adapter.chat(request).then((response) => {
+            expect(response).toBeDefined();
+        });
+
+        polly.stop();
+
+        expect(response).toMatchInlineSnapshot();
+
+    })
 })
