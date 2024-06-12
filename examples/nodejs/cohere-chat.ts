549e1887ecc5b8302a6ae0c803fcd3cc9fb8baba
Monarch Wadia <monarchwadia@gmail.com>
2024-06-12 01:24:31 -0400

    Updated cohere chat example

--- examples/nodejs/cohere-chat.ts
+++ examples/nodejs/cohere-chat.ts
@@ -1,13 +1,12 @@
-/*
- * A simple example with Cohere
- */
-
 import { config } from 'dotenv';
 config();
-import { Chat } from "ragged/chat"
+import { Chat } from "ragged/chat";
 
 const c = Chat.with('cohere', { apiKey: process.env.COHERE_API_KEY, model: 'command-light' });
 
-const messages = await c.chat('What is a rickroll?');
+async function getResponse() {
+    const messages = await c.chat('What is a rickroll?');
+    console.log(messages.at(-1)?.text); // Prints the response
+}
 
-console.log(messages.at(-1)?.text); // A rickroll is ...
+getResponse().catch(e => console.error(e));
