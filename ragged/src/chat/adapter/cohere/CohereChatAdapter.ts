4e288f3bf9392869cf5d18c8cd4452386198b268
Monarch Wadia <monarchwadia@gmail.com>
2024-06-12 00:36:34 -0400

    Added not implemented status for tools in Cohere

--- ragged/src/chat/adapter/cohere/CohereChatAdapter.ts
+++ ragged/src/chat/adapter/cohere/CohereChatAdapter.ts
@@ -10,6 +10,9 @@ export class CohereChatAdapter implements BaseChatAdapter {
     constructor(private apiClient: ApiClient, private config: CohereChatAdapterConfig) { }
 
     async chat(request: ChatRequest): Promise<ChatResponse> {
+        if (request.tools) {
+            throw new Error("Not implemented. Currently, Ragged does not support tools in Cohere requests.");
+        }
         const cohereRequest = CohereChatMapper.mapChatRequestToCohereRequest(request);
 
         const response = await this.apiClient.post('https://api.cohere.com/v1/chat', {
