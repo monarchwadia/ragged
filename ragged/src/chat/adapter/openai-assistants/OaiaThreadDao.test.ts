26b1be4577ed5f9a5e501a3199bbc2a405c52b46
Monarch Wadia <monarchwadia@gmail.com>
2024-06-19 23:47:26 -0400

    OaiaThreadDao

---
+++ ragged/src/chat/adapter/openai-assistants/OaiaThreadDao.test.ts
@@ -0,0 +1,32 @@
+import { startPollyRecording } from "../../../../test/startPollyRecording";
+import { ApiClient } from "../../../support/ApiClient";
+import { OaiaThreadDao } from "./OaiaThreadDao";
+
+describe("OaiaThreadDao", () => {
+  describe("createThread", () => {
+    it("can be created", async () => {
+      const apiClient = new ApiClient();
+      const oaiaThreadDao = new OaiaThreadDao(apiClient);
+
+      const polly = startPollyRecording(
+        "OaiaThreadDao > createThread > can be created"
+      );
+
+      const thread = await oaiaThreadDao.createThread(
+        process.env.OPENAI_API_KEY as string
+      );
+
+      polly.stop();
+
+      expect(thread).toMatchInlineSnapshot(`
+        {
+          "created_at": 1718855111,
+          "id": "thread_mGG7FJ133ssPowO2UisHkL3h",
+          "metadata": {},
+          "object": "thread",
+          "tool_resources": {},
+        }
+      `);
+    });
+  });
+});
--- ragged/src/chat/adapter/openai-assistants/OaiaThreadDao.ts
+++ ragged/src/chat/adapter/openai-assistants/OaiaThreadDao.ts
@@ -1,8 +1,16 @@
 import { ApiClient } from "../../../support/ApiClient";
-import { OaiaConfigCommons } from "./Oaia.types";
+import { ThreadCreateResponseBody } from "./OaiaThreadDaoTypes"
 
 export class OaiaThreadDao {
-    constructor(private ctx: OaiaConfigCommons, private apiClient: ApiClient) {
+    constructor(private apiClient: ApiClient) { }
 
+    createThread(apiKey: string): Promise<ThreadCreateResponseBody> {
+        return this.apiClient.post("https://api.openai.com/v1/threads", {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${apiKey}`,
+                "OpenAI-Beta": "assistants=v2"
+            }
+        });
     }
 }
