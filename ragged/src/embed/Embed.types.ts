3b44c2f771e79fdae20dbbc562968a455b616e98
Monarch Wadia <monarchwadia@gmail.com>
2024-06-21 01:46:52 -0400

    Added friendly comments to embed type

--- ragged/src/embed/Embed.types.ts
+++ ragged/src/embed/Embed.types.ts
@@ -30,6 +30,18 @@ export type EmbeddingResponse = {
     embedding: number[];
 }
 
+/**
+ * Creates an embedding for the given text using a specific provider.
+ * 
+ * This function takes some text and sends it to an API to get an embedding.
+ * An embedding is like a special set of numbers that represents the text in a way
+ * that computers can understand and work with. These numbers can be used for things
+ * like finding similar texts, grouping texts, and more.
+ * 
+ * @param request - An object with the text you want to embed and optional information about the model to use.
+ * @returns A promise that gives back an object with the model, provider, and the embedding data (the special numbers).
+ * @throws Error if the API request fails.
+ */
 export interface BaseEmbeddingAdapter {
     embed(request: EmbeddingRequest): Promise<EmbeddingResponse>;
 }
