15f7b219a87fbfbdfaa92ed485acf9c1986e49d3
Monarch Wadia <monarchwadia@gmail.com>
2024-06-21 02:15:43 -0400

    Openai Assistants is now being exposed via public interface

---
+++ ragged/src/public/chat/adapter/openai-assistants.ts
@@ -0,0 +1,4 @@
+export { OaiaChatAdapter } from "../../../chat/adapter/openai-assistants/adapter/OaiaChatAdapter";
+export type { OaiaChatAdapterConstructorOpts, OpenaiAssistantsChatAdapterConfig } from "../../../chat/adapter/openai-assistants/adapter/OaiaChatAdapter";
+export { provideOpenaiAssistantsChatAdapter } from "../../../chat/adapter/openai-assistants/provideOpenaiAssistantsChatAdapter";
+export type { OpenaiAssistantsChatProviderParam } from "../../../chat/adapter/openai-assistants/provideOpenaiAssistantsChatAdapter";
