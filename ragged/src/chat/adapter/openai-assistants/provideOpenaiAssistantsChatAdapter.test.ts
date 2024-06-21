fb365ae5c18a4c2ddf005a75cb6033ef08b900ae
Monarch Wadia <monarchwadia@gmail.com>
2024-06-20 21:54:12 -0400

    Added a simple adapter test

---
+++ ragged/src/chat/adapter/openai-assistants/provideOpenaiAssistantsChatAdapter.test.ts
@@ -0,0 +1,19 @@
+import { provideOpenaiAssistantsChatAdapter } from "./provideOpenaiAssistantsChatAdapter";
+
+describe("provideOpenaiAssistantsChatAdapter", () => {
+    it("can instantiate", () => {
+        const adapter = provideOpenaiAssistantsChatAdapter({
+            config: {
+                // apiKey: process.env.OPENAI_API_KEY as string,
+                apiKey: "not-real",
+                assistant: {
+                    description: "test",
+                    instructions: "test",
+                    model: "gpt-3.5-turbo",
+                    name: "test-agent"
+                }
+            }
+        });
+        expect(adapter).toBeDefined();
+    });
+});
