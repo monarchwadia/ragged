0da44b1a3d1cded9adbd359e34c9f35e001b985d
Monarch Wadia <monarchwadia@gmail.com>
2024-06-25 12:41:32 -0400

    Added openai adapter test

--- ragged/src/chat/adapter/azure-openai/provideAzureOpenaiChatAdapter.test.ts
+++ ragged/src/chat/adapter/azure-openai/provideAzureOpenaiChatAdapter.test.ts
@@ -1,5 +1,15 @@
+import { provideAzureOpenAiChatAdapter } from "./provideAzureOpenaiChatAdapter";
+
 describe("provideAzureOpenaiChatAdapter", () => {
     it('can instantiate', () => {
-
+        const adapter = provideAzureOpenAiChatAdapter({
+            config: {
+                apiKey: 'test',
+                apiVersion: 'test',
+                deploymentName: 'test',
+                resourceName: 'test'
+            }
+        });
+        expect(adapter).toBeDefined();
     });
 });
