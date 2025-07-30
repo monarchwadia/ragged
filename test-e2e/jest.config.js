2aa8bfe61c3b0bd3849351678af3661d2c4678ce
Monarch Wadia <monarchwadia@gmail.com>
2025-07-30 22:54:57 +0000

    added e2e test folder

---
+++ test-e2e/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // verbose: true,
+  // Jest 29 doesn't support Prettier 3
+  // See docs for more details. https://jestjs.io/docs/configuration/#prettierpath-string
+  prettierPath: require.resolve('prettier-2'),
+  setupFiles: ["dotenv/config"]
+};
